65b84c9d510d92df5382878b3b7e7d6e09dcaba4
RicketyRocks <rick.shelton@gmail.com>
2017-08-28 18:04:40 -0500

    get replica number from hostname with specific format

--- src/main/java/snowflake/Snowflake.java
+++ src/main/java/snowflake/Snowflake.java
@@ -14,13 +14,15 @@ public class Snowflake {
 		String numReplicasStr = bundle.getString("snowflake.number.of.replicas");
 		int numReplicas = Integer.parseInt(numReplicasStr);
 		String myName = "unknown";
+		int myNumber = 0;
 		try {
 			myName = InetAddress.getLocalHost().getHostName();
+			myNumber = Integer.parseInt(myName.substring(myName.lastIndexOf('-')+1));
 		} catch (UnknownHostException ex) {
 			System.out.println("Hostname can not be resolved");
 		}
-		String msgFmt = "hello, world. My name is %s, and I am 1 of %d";
-		String msg = String.format(msgFmt, myName, numReplicas);
+		String msgFmt = "hello, world. My name is %s, and I am %d of %d";
+		String msg = String.format(msgFmt, myName, myNumber, numReplicas);
 		
 		while(true) {
 			System.out.println(msg);
