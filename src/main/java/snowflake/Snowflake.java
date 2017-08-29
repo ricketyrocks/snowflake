84a3717ea316e01c0cba2ec685140ca97aef0466
RicketyRocks <rick.shelton@gmail.com>
2017-08-28 21:49:26 -0500

    make replicas self-identify; cannot scale, but why scale when replicas cannot chew what they bite off?

--- src/main/java/snowflake/Snowflake.java
+++ src/main/java/snowflake/Snowflake.java
@@ -1,30 +1,33 @@
 package snowflake;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.ResourceBundle;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Snowflake {
 	private static Logger log = LoggerFactory.getLogger(Snowflake.class);
+
 	public static void main(String[] args) throws InterruptedException {
 
-		ResourceBundle bundle = ResourceBundle.getBundle("snowflake");
-		String numReplicasStr = bundle.getString("snowflake.number.of.replicas");
+		String numReplicasStr = System.getenv("TOTAL_REPLICAS");
+		// ResourceBundle bundle = ResourceBundle.getBundle("snowflake");
+		// String numReplicasStr =
+		// bundle.getString("snowflake.number.of.replicas");
 		int numReplicas = Integer.parseInt(numReplicasStr);
-		String myName = "unknown";
 		int myNumber = 0;
+		String myName = "unknown";
 		try {
 			myName = InetAddress.getLocalHost().getHostName();
-			myNumber = Integer.parseInt(myName.substring(myName.lastIndexOf('-')+1));
+			myNumber = 1 + Integer.parseInt(myName.substring(myName.lastIndexOf('-') + 1));
 		} catch (UnknownHostException ex) {
 			System.out.println("Hostname can not be resolved");
 		}
 		String msgFmt = "hello, world. My name is %s, and I am %d of %d";
 		String msg = String.format(msgFmt, myName, myNumber, numReplicas);
-		
-		while(true) {
+
+		while (true) {
 			System.out.println(msg);
 			log.info(msg);
 			Thread.sleep(5000);
